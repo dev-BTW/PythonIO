a814396e7a5d94d551f901d291b51ff60e3e732b
dev-BTW <parth.more.2304@gmail.com>
2024-06-12 09:47:31 +0530

    modified read7

--- read7.py
+++ read7.py
@@ -1,4 +1,25 @@
+countries ={}
+
 with open('country_info.txt','r',encoding='utf-8') as text:
     for row in text:
-        line = row.strip('\n').split('|')
-        print(line)
+        data = row.strip('\n').split('|')
+        country,capital,code,code3,dialing,timeZone,currency=data
+        #print(country,capital,code,code3,dialing,timeZone,currency,sep='\n\t')
+        country_dict ={
+            'name':country,
+            'capital':capital,
+            'code':code,
+            'code3':code3,
+            'dialing':dialing,
+            'timeZone':timeZone,
+            'currency':currency,
+        }
+        #print(country_dict)
+        countries[country.casefold()] = country_dict
+
+#print(countries)
+
+choosenCountry = input("Enter a country name: ").casefold
+if choosenCountry in countries:
+    countryData = countries[choosenCountry]
+    print(countryData)
